4276a783361c52a12c7b50d40cf507af34f7a74e
Brett Holman <bpholman5@gmail.com>
2021-04-21 17:52:17 -0600

    started rust 2d array implementation

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -1,50 +1,56 @@
+extern crate csv;
 
-/*
-fn init_new(size: u8) -> &'static mut [&'static mut [i32]] {// Box<[u8]>{
-    //println!("{:?}",x);
-    /*
-    let x: [Box<[u8]>; size] = [
-        for i in 0..size {
-            Box::new([0,0,0]);
-        }
-        //Box::new([1,1,1]),
-        //Box::new([0,0,0]),
-    ];
-    return x
-    */
-    //let (height, width) = (size, size);
-    let height = size;
-    let width = size;
-    let mut oned = vec![0; (height * width).into()];
-    let mut slices: Vec<_> = oned.as_mut_slice().chunks_mut(width.into()).collect();
-    let grid = slices.as_mut_slice();
-}
-*/
+use std::fs::File;
+
+#[allow(dead_code)]
 
 fn main() {
-    const WIDTH: usize = 3;
-    const HEIGHT: usize = 3;//(size, size);
-    let mut arr = vec![0; HEIGHT*WIDTH];
-    set_vec(&mut arr, WIDTH, 1, 1, 1);
-    print_vec(&arr,HEIGHT,WIDTH);
-    println!("changing value: {} to 0", set_vec(&mut arr, WIDTH, 1, 1, 0));
-    print_vec(&arr,HEIGHT,WIDTH);
+    let mut a = Arr2D::new(3);
+    a.print();
+    println!("changing value: {} to 1", a.set(1, 1, 1));
+    println!();
+    a.print();
 }
 
-//fn set_vec(v: &std::vec::Vec<u8>, width: usize, x: usize, y: usize, val: u8) -> u8{
-fn set_vec(v: &mut std::vec::Vec<u8>, width: usize, x: usize, y: usize, val: u8) -> u8{
-    let old = get_vec(v.to_vec(), width, x, y);
-    v[width*x + y] = val;
-    old
+struct Arr2D {
+    arr: std::vec::Vec<u8>,
+    width: usize,
 }
-fn get_vec(v: Vec<u8>, width: usize, x: usize, y: usize) -> u8{
-    v[width*x + y]
-}
-fn print_vec(v: &Vec<u8>, height: usize, width: usize){
-    for i in 0..width {
-        for j in 0..height{
-           print!("{} ", v[width*i + j]); 
+
+impl Arr2D {
+    fn new(size: usize) -> Arr2D {
+        Arr2D{
+            width: size,
+            arr: vec![0; size*size],
         }
-        println!();
+    }
+
+    fn load(file: String){
+        let file = File::open(file).expect("Unable to open");
+        let mut rdr = csv::Reader::from_reader(file);
+        for result in rdr.records() {
+            let record = result;
+            println!("{:?}", record);
+        }
+    }
+
+    fn get(&self, x: usize, y: usize) -> u8{
+        self.arr[self.width*x + y]
+    }
+
+    // return old value before setting
+    fn set(&mut self, x: usize, y: usize, val: u8) -> u8{
+        let old = self.get(x, y);
+        self.arr[self.width * x + y] = val;
+        old
+    }
+
+    fn print(&self){
+        for i in 0..self.width {
+            for j in 0..self.width{
+                print!("{} ", self.arr[self.width*i + j]);
+            }
+            println!();
+        }
     }
 }
