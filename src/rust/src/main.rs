1503f6bc5ae324bfd7e7d8821c413ccc66b59a24
Brett Holman <bpholman5@gmail.com>
2021-06-03 21:57:27 -0600

    completed csv parsing code

--- src/rust/src/main.rs
+++ src/rust/src/main.rs
@@ -1,23 +1,36 @@
 extern crate csv;
+extern crate serde_derive;
 
+use serde::Deserialize;
 use std::fs::File;
+use std::error::Error;
+use csv::ReaderBuilder;
 
-#[allow(dead_code)]
-
-fn main() {
-    let mut a = Arr2D::new(3);
-    a.print();
-    println!("changing value: {} to 1", a.set(1, 1, 1));
-    println!();
-    a.print();
+#[derive(Debug, Deserialize, Eq, PartialEq)]
+struct Row {
+    values: Vec<u8>,
 }
 
+fn main() {
+    let mut a = Arr2D::default();
+    if let Ok(_) = a.new_load("../../../data/flier".to_string()) {
+        a.print();
+    }
+
+    let mut b = Arr2D::new(3);
+    b.print();
+    println!("changing value: {} to 1\n", b.set(1, 1, 1));
+    b.print();
+}
+
+#[derive(Debug, Default)]
 struct Arr2D {
     arr: std::vec::Vec<u8>,
     width: usize,
 }
 
 impl Arr2D {
+
     fn new(size: usize) -> Arr2D {
         Arr2D{
             width: size,
@@ -25,27 +38,46 @@ impl Arr2D {
         }
     }
 
-    fn load(file: String){
+    fn new_load(&mut self, file: String) -> Result<(), Box<dyn Error>> {
         let file = File::open(file).expect("Unable to open");
-        let mut rdr = csv::Reader::from_reader(file);
-        for result in rdr.records() {
-            let record = result;
-            println!("{:?}", record);
+        let mut rdr = ReaderBuilder::new()
+            .has_headers(false)
+            .delimiter(b';')
+            .from_reader(file);
+        let mut i = rdr.deserialize();
+
+        let mut x;
+        let mut y = 0;
+        while let Some(result) = i.next() {
+            let record: Row = result?;
+            if self.width == 0 {
+                self.width = record.values.len();
+                self.arr = vec![0; self.width*self.width];
+
+            }
+            self.width = record.values.len();
+            x=0;
+            for item in record.values.clone(){
+                self.set(x, y, item);
+                x+=1;
+            }
+            y+=1;
         }
+        Ok(())
     }
 
-    fn get(&self, x: usize, y: usize) -> u8{
+    fn get(&self, x: usize, y: usize) -> u8 {
         self.arr[self.width*x + y]
     }
 
     // return old value before setting
-    fn set(&mut self, x: usize, y: usize, val: u8) -> u8{
+    fn set(&mut self, x: usize, y: usize, val: u8) -> u8 {
         let old = self.get(x, y);
         self.arr[self.width * x + y] = val;
         old
     }
 
-    fn print(&self){
+    fn print(&self) {
         for i in 0..self.width {
             for j in 0..self.width{
                 print!("{} ", self.arr[self.width*i + j]);
